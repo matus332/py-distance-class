fa39c14d9b68a8e94d66a6e6515c040cb2c9149e
Denys Matusevych <denytop3@gmail.com>
2026-03-14 23:16:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,51 @@
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Any) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        else:
+            return NotImplemented
+
+    def __iadd__(self, other: Any) -> Distance:
+        if isinstance(other, Distance):
+            other_km = other.km
+        elif isinstance(other, (int, float)):
+            other_km = other
+        else:
+            return NotImplemented
+        self.km += other_km
+        return self
+
+    def __mul__(self, other: Any) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: Any) -> Distance:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: Any) -> Distance:
+        return self.km < other
+
+    def __gt__(self, other: Any) -> Distance:
+        return self.km > other
+
+    def __eq__(self, other: Any) -> Distance:
+        return self.km == other
+
+    def __le__(self, other: Any) -> Distance:
+        return self.km <= other
+
+    def __ge__(self, other: Any) -> Distance:
+        return self.km >= other
